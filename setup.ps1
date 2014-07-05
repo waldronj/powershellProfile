83f4868f9cef570401bc097cc7272c2884f48052
waldronj <jwaldron@thejwal.com>
2014-07-05 18:38:15 -0500

    Remove the , then recreate it

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,7 @@
+<#
+This script is intended to wipe then recreate the $Profile file. Any changes made locally will be wiped out
+UNLESS you add the changes back to the setup.ps1 file that will reinstate them when the script is ran again
+#>
+remove-item $profile
 new-item -type file -force $profile
 add-content $profile "set-alias c clear"
